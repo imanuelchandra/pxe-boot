51bd7abb7211b34f67c82cd2ec7834653873aa7e
Imanuel Chandra <chandra.lefta@gmail.com>
2023-10-11 18:23:26 +0900

    update config

--- scripts/3.bootdir.sh
+++ scripts/3.bootdir.sh
@@ -2,13 +2,13 @@
 
 # sudo chown pxeadmin:pxeadmin /data
 # sudo chmod -R 777 /data
-mkdir /home/pxeadmin/boot
+mkdir /home/pxeadmin/pxe_root
 sudo bindfs --force-user=pxeadmin --force-group=pxeadmin \
     --create-for-user=6565 --create-for-group=6565 \
     --chown-ignore --chgrp-ignore \
-    /data /home/pxeadmin/boot
+    /data/pxe_root /home/pxeadmin/pxe_root
 
 echo "===================================\n"
-echo "Boot Directory \n"
+echo "PXE Root Directory \n"
 echo "===================================\n"
-ls -lah /home/pxeadmin/boot
+ls -lah /home/pxeadmin/pxe_root
